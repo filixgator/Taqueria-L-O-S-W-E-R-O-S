cee39c310ad13b8b6959c50809c071dbb61a26a6
Andres Marin <avmt97@gmail.com>
2017-11-29 10:04:14 -0800

    Unicamente falta la tabla

--- prueba.py
+++ prueba.py
@@ -5,14 +5,15 @@ import numpy as np
 
 plt.figure(1, figsize=(18, 5))
 #Datos de prueba
-diccTacos = {'Mulitas':35,'Quesadillas':43,'Tacos':41,'Tortas':13,'Tostadas':39}
-diccIngredientes = {"CEBOLLA" : [500,400,350,300], "CILANTRO" : [500,450,400], "SALSA" : 500, "GUACAMOLE" : 500, "QUESO" : 500, "FRIJOLES" : 500}
+diccTacos = {'Mulitas':35,'Quesadillas':43,'Tacos':41,'Tortas':13,'Tostadas':39, 'PMulitas':35,'PQuesadillas':43,'PTacos':41,'PTortas':13,'PTostadas':39}
+diccIngredientes = {"CEBOLLA" : [500,400,350,300], "CILANTRO" : [500,450,400], "SALSA" : [500,480,430], "GUACAMOLE" : [500,470,420], "QUESO" : [500,300,250], "FRIJOLES" : [500,450,300]}
 diccTiempo = {"CEBOLLA" : 500, "CILANTRO" : 500}
 
 
 def graficar(diccTacos,diccIngredientes):
 	names = ['Mul','Ques','Tacos','Tortas','Tost']
 	values = [diccTacos['Mulitas'],diccTacos['Quesadillas'],diccTacos['Tacos'],diccTacos['Tortas'], diccTacos['Tostadas']]
+	valuesP = [diccTacos['PMulitas'],diccTacos['PQuesadillas'],diccTacos['PTacos'],diccTacos['PTortas'], diccTacos['PTostadas']]
 
 	#Cantidad de tacos
 	plt.subplot(231)
@@ -24,20 +25,27 @@ def graficar(diccTacos,diccIngredientes):
 
 	#Promedio por orden
 	plt.subplot(233)
-	plt.scatter(names, values)
+	plt.bar(names, valuesP)
 
 	#Ingredientes
 	plt.subplot(234)
-	for i in diccIngredientes.keys():
-		plt.plot(diccIngredientes[i],label=i)
-
+	plt.plot(diccIngredientes["CEBOLLA"],label="CEB")
+	plt.plot(diccIngredientes["CILANTRO"],label="CIL")
+	plt.plot(diccIngredientes["SALSA"],label="SAL")
+	plt.plot(diccIngredientes["GUACAMOLE"],label="GUA")
+	plt.plot(diccIngredientes["QUESO"],label="QUE")
+	plt.plot(diccIngredientes["FRIJOLES"],label="FRI")
 
 	#Tabla tamanio ordenes
+	n_rows = diccTiempo.keys()
+	cell_text = [n_rows]
 	plt.subplot(235)
-#	plt.table(names, values)
-
+	plt.axis('off')
+	plt.table(cellText=cell_text)
 
 	plt.suptitle('Taqueria los WERS')
 	plt.show()
 
 graficar(diccTacos,diccIngredientes)
+#print(len(diccTiempo))
+
